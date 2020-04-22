e7f932d7b9ad72a4bae651abd651e9de4587ff83
Matt Dale <MatthewDale@MSQSND.com>
2020-04-22 13:35:11 -0400

    added potential speed up lambda func

--- intermod_v4.py
+++ intermod_v4.py
@@ -2,6 +2,13 @@ import collections
 import itertools
 import random
 
+"""
+given_value = 2
+a_list = [1, 5, 8]
+absolute_difference_function = lambda list_value : abs(list_value - given_value)
+closest_value = min(a_list, key=absolute_difference_function)
+"""
+
 
 class IMD(object):
     """
